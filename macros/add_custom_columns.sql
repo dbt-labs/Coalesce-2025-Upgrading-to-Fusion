39e22e088f4072c263259b5a5b7da8e6c590334f
max tijdeman <max.tijdeman@dbtlabsworkshop.com>
2025-10-14 22:00:42 +0000

    latest changes

--- macros/add_custom_columns.sql
+++ macros/add_custom_columns.sql
@@ -8,11 +8,9 @@
   {#- These config.get() calls will return None in Fusion since custom configs must be under 'meta' -#}
   {% set add_row_number = config.get('meta').add_row_number %}
   {% set add_hash_key = config.get('meta').add_hash_key %}
-  {% set business_unit = config.get('meta').business_unit %}
   {% set enable_audit_fields = config.get('meta').enable_audit_fields %}
   
-  {#- This will cause a compilation error when business_unit is None -#}
-  {% set business_unit_upper = business_unit.upper() %}
+ 
   
   {#- Generate additional computed columns based on config -#}
   {% if add_row_number %}
@@ -23,9 +21,7 @@
     , md5(cast(id as string) || cast(customer as string)) as record_hash
   {% endif %}
   
-  {% if business_unit %}
-    , '{{ business_unit_upper }}' as business_unit
-  {% endif %}
+
   
   {% if enable_audit_fields %}
     , current_timestamp() as processed_at
