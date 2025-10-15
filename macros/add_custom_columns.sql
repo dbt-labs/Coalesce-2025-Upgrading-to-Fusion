9407f822fd5f01dc5d4a790d2db6d014dff4dd57
sory cisse <sory.cisse@dbtlabsworkshop.com>
2025-10-15 17:40:47 +0000

    testing

--- macros/add_custom_columns.sql
+++ macros/add_custom_columns.sql
@@ -6,13 +6,12 @@
   -#}
   
   {#- These config.get() calls will return None in Fusion since custom configs must be under 'meta' -#}
-  {% set add_row_number = config.get('add_row_number') %}
-  {% set add_hash_key = config.get('add_hash_key') %}
-  {% set business_unit = config.get('business_unit') %}
-  {% set enable_audit_fields = config.get('enable_audit_fields') %}
+  {% set add_row_number = config.get('meta').add_row_number %}
+  {% set add_hash_key = config.get('meta').add_hash_key %}
+  {% set enable_audit_fields = config.get('meta').enable_audit_fields %}
   
   {#- This will cause a compilation error when business_unit is None -#}
-  {% set business_unit_upper = business_unit.upper() %}
+--   {% set business_unit_upper = business_unit.upper() %}
   
   {#- Generate additional computed columns based on config -#}
   {% if add_row_number %}
@@ -23,9 +22,9 @@
     , md5(cast(id as string) || cast(customer as string)) as record_hash
   {% endif %}
   
-  {% if business_unit %}
-    , '{{ business_unit_upper }}' as business_unit
-  {% endif %}
+--   {% if business_unit %}
+--     , '{{ business_unit_upper }}' as business_unit
+--   {% endif %}
   
   {% if enable_audit_fields %}
     , current_timestamp() as processed_at
